74729a0146e8164985421a849148d4ce2927e894
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-07-10 19:51:55 -0300

    Implementar API feedHome

--- Repository/IPublicacaoRepository.cs
+++ Repository/IPublicacaoRepository.cs
@@ -1,9 +1,13 @@
-﻿using DevagramCSharp.Models;
+﻿using DevagramCSharp.Dtos;
+using DevagramCSharp.Models;
 
 namespace DevagramCSharp.Repository
 {
     public interface IPublicacaoRepository
     {
         public void Publicar(Publicacao publicacao);
+        List<FeedRespostaDto> GetPublicacoesFeed(int idUsuario);
+        List<FeedRespostaDto> GetPublicacoesFeedUsuario(int idUsuario);
+        int GetQtdePublicacoes(int idUsuario);
     }
 }
